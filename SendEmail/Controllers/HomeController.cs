9631f6e721e203d2c895d2a82f6e6f4ceb326bc9
Sohrab Gheisari <sohrabgheisari@hotmail.com>
2022-04-17 21:54:53 +0430

    init

---
+++ SendEmail/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendEmail.Infrastructure;
+
+namespace SendEmail.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly EmailService _emailService;
+        public HomeController(ILogger<HomeController> logger, EmailService emailService)
+        {
+            _logger = logger;
+            _emailService = emailService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await _emailService.SendEmailAsync("destination-email@gmail.com", "visit codecell", "visit codecell on codecell.ir");
+            return View();
+        }
+    }
+}
---
+++ SendEmail/Infrastructure/EmailService.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.Extensions.Options;
+
+namespace SendEmail.Infrastructure
+{
+    public class EmailService
+    {
+        private readonly EmailSetting _emailSetting;
+
+        public EmailService(IOptions<EmailSetting> emailSetting)
+        {
+            _emailSetting = emailSetting.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            try
+            {
+                // Credentials
+                var credentials = new NetworkCredential(_emailSetting.Sender, _emailSetting.Password);
+
+                // Mail message
+                var mail = new MailMessage
+                {
+                    From = new MailAddress(_emailSetting.Sender, _emailSetting.SenderName),
+                    Subject = subject,
+                    Body = message,
+                    IsBodyHtml = true
+                };
+
+                mail.To.Add(new MailAddress(email));
+
+                // Smtp client
+                var client = new SmtpClient
+                {
+                    Port = _emailSetting.MailPort,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Host = _emailSetting.MailServer,
+                    EnableSsl = _emailSetting.Sender.Contains("gmail"),
+                    Credentials = credentials
+                };
+
+                // Send it...         
+                await client.SendMailAsync(mail);
+            }
+            catch (Exception ex)
+            {
+                // TODO: handle exception
+
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+    }
+}
